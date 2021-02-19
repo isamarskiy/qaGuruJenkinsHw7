70c20a53254caef0c2247766e204ecf87d0f91d0
samarskiy <samarskyii@gmail.com>
2021-02-19 13:55:10 +0300

    some fix

--- src/test/java/SampleTestsForJenkins.java
+++ src/test/java/SampleTestsForJenkins.java
@@ -1,19 +1,20 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class SampleTestsForJenkins {
 
     @Test
     @DisplayName("Positive test")
     void positiveTest(){
-        assertEquals(4, 4);
+        assertThat(4, is(4));
     }
 
     @Test
     @DisplayName("Negative test")
     void negativeTest(){
-        assertEquals(4, 5);
+        assertThat(4, is(5));
     }
 }
